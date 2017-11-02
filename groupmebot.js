56b3ce2ca7a7a153452407c5cce2f45e936bd5b9
EC2 Default User <ec2-user@ip-172-31-80-142.ec2.internal>
2017-11-02 01:01:23 +0000

    Made a bot for the chapter groupme

---
+++ groupmebot.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var request = require('request');
+var app = express()
+
+function sendMessage(res) {
+    var jsonRes = {
+	"bot_id":"<bot_id>",
+	"text":res
+    };
+    request.post(' https://api.groupme.com/v3/bots/post', {json: jsonRes}, function(error, response, body){
+	if (!error && response.statusCode == 200) {
+	    console.log(body);
+	}
+    });
+}
+
+app.post('/slackbot', function(req, res){
+    console.log('Received a POST request.');
+    req.on('data', function(chunk) {
+	var message = JSON.parse(chunk.toString());
+
+	if (message.text === "@testbank") {
+	    console.log("Message received: " + message.text);
+	    testBankResponse = "<Test Bank: Test Bank usernames and passwords>"
+
+	    sendMessage(testBankResponse);
+	}
+
+	if (message.text === "@carport") {
+	    console.log("Message received: " + message.text);
+	    carPortResponse = "Carport: <car port map link>";
+	    
+	    sendMessage(carPortResponse);
+	}
+
+	if (message.text === "@wifi") {
+	    console.log("Message received: " + message.text);
+	    wifiResponse = "Wifi: <network names and passwords>";
+	    
+	    sendMessage(wifiResponse);
+	}
+
+	if (message.text === "@pledges") {
+	    console.log("Message received: " + message.text);
+	    pledgesResponse = "Pledges: <link to list of new members>";
+
+	    sendMessage(pledgesResponse);
+	}
+    });
+});
+
+app.listen(6969, function(){
+    console.log("Listening on Port <port_num>...");
+});
